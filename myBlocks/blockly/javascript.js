c6a990cff007ab4aaee86a798d10cd7c79bfe528
ouo405200 <74400618+ouo405200@users.noreply.github.com>
2020-11-13 16:02:03 +0800

    Add files via upload

---
+++ myBlocks/blockly/javascript.js
@@ -0,0 +1,6 @@
+Blockly.JavaScript['sayhello'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'hay';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
